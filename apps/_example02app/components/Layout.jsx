13fb818f27a8b553b10ad9756e75a41424c22d8a
Pahaz <pahaz.white@gmail.com>
2020-04-21 17:11:01 +0300

    README: add next.js advanced example

---
+++ apps/_example02app/components/Layout.jsx
@@ -0,0 +1,18 @@
+import Header from "./Hearer";
+import Head from 'next/head'
+
+const layoutStyle = {
+    margin: `20px`,
+};
+
+const Layout = (props) => (<>
+    <Head>
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header/>
+    <div style={layoutStyle}>
+        {props.children}
+    </div>
+</>);
+
+export default Layout;
