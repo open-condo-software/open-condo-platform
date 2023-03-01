open-condo-software/condo

refactor(condo): DOMA-2567 refactored hints

apps/condo/pages/ticket/[id]/index.tsx
@@ -386,12 +386,14 @@ export const TicketPageContent = ({ ticket, refetchTicket, loading, organization
                                                 </Col>
                                             )
                                         }
-                                        <IncidentHints
-                                            organizationId={organization.id}
-                                            propertyId={ticketPropertyId}
-                                            dateISO={ticket.createdAt}
-                                            classifier={ticket.classifier}
-                                        />
+                                        <Col span={24}>
+                                            <IncidentHints
+                                                organizationId={organization.id}
+                                                propertyId={ticketPropertyId}
+                                                dateISO={ticket.createdAt}
+                                                classifier={ticket.classifier}
+                                            />
+                                        </Col>
                                     </Row>
                                 </Col>
                                 <ActionBar>

open-condo-software/open-condo-platform

apps/condo/pages/ticket/[id]/index.tsx
@@ -409,12 +409,14 @@ export const TicketPageContent = ({ ticket, refetchTicket, loading, organization
                                     </Col>
                                 )
                             }
-                            <IncidentHints
-                                organizationId={organization.id}
-                                propertyId={ticketPropertyId}
-                                dateISO={ticket.createdAd}
-                                classifier={ticket.classifier}
-                            />
+                            <Col span={24}>
+                                <IncidentHints
+                                    organizationId={organization.id}
+                                    propertyId={ticketPropertyId}
+                                    dateISO={ticket.createdAd}
+                                    classifier={ticket.classifier}
+                                />
+                            </Col>
                         </Row>
                     </Col>
                     <ActionBar>